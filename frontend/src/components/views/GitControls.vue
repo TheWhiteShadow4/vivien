7df5b23a5f1120c51192e74188a1f8b5b4ab0a98
TheWhiteShadow <thewhiteshadow87@gmail.com>
2026-09-01 10:51:31 +0200

    Plus/Minus Icon hinzugefügt.

--- frontend/src/components/views/GitControls.vue
+++ frontend/src/components/views/GitControls.vue
@@ -1,13 +1,10 @@
 <!-- src\components\views\GitControls.vue -->
 <script setup lang="ts">
 import IconGitCommit from '@/icons/IconGitCommit.vue'
-import IconGitPull from '@/icons/IconGitPull.vue'
 import IconGitPush from '@/icons/IconGitPush.vue'
-import IconPopStash from '@/icons/IconPopStash.vue'
 import IconPushStash from '@/icons/IconPushStash.vue'
 import { useStore } from '@/store/index.ts'
 import { computed } from 'vue'
-import CounterBadge from '../base/CounterBadge.vue'
 import ListButton from '../base/ListButton.vue'
 
 const store = useStore();
@@ -16,32 +13,6 @@ const emit = defineEmits(["git"]);
 
 const gitChangeCount = computed(() => {
 	return store.stage ? (store.stage?.added + store.stage?.removed) : 0;
-});
-const commitStyle = computed(() => {
-	if (gitChangeCount.value > 0)
-	{
-		return "font-bold bg-gradient-to-t from-vit-primary to-vit-primary2 hover:bg-gradient-to-b";
-	}
-	else
-	{
-		return "text-vit-text-muted";
-	}
-});
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const pushStyle = computed(() => {
-	if (store.git && store.git.remote)
-	{
-		if (store.git.remote.behindCount || store.git.untracked.length > 0)
-		{
-			return "font-bold bg-gradient-to-t from-vit-primary to-vit-primary2";
-		}
-		else
-		{
-			return "text-vit-text-muted";
-		}
-	}
-	return "text-vit-text-muted";
 });
 
 
@@ -57,12 +28,6 @@ withDefaults(defineProps<Props>(), {
 
 <template>
 	<nav class="flex flex-col gap-2">
-		<!-- <button @click="emit('git', 'fetch')"
-			:class="[GitButtonBase, GitButtonNormal]">
-			<IconGitPull :class="[BaseIconStyle, FetchIconStyle]" />
-			<span v-if="variant == 'full'" class="ml-3">Fetch</span>
-		</button> -->
-
 		<ListButton
 		:variant="gitChangeCount ? 'primary' : 'normal'"
 		color="accent2"
--- frontend/src/components/views/Toolbar.vue
+++ frontend/src/components/views/Toolbar.vue
@@ -1,8 +1,27 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <!-- src/components/views/Toolbar.vue -->
 <script setup lang="ts">
+import IconPlus from '@/icons/IconPlus.vue';
+import BaseIconButton from '../base/BaseIconButton.vue';
+import IconMinus from '@/icons/IconMinus.vue';
+import IconUpload from '@/icons/IconUpload.vue';
+import IconDownload from '@/icons/IconDownload.vue';
 
 </script>
 
 <template>
-	
+	<div>
+	<BaseIconButton>
+		<IconPlus />
+	</BaseIconButton>
+	<BaseIconButton>
+		<IconMinus />
+	</BaseIconButton>
+	<BaseIconButton>
+		<IconUpload />
+	</BaseIconButton>
+		<BaseIconButton>
+		<IconDownload />
+	</BaseIconButton>
+	</div>
 </template>
