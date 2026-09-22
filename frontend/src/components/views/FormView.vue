1c126d5c52ec2e14e68768bc6ef400a63b81e0c1
TheWhiteShadow <thewhiteshadow87@gmail.com>
2026-09-22 16:37:42 +0200

    json schema tests

---
+++ frontend/src/components/views/FormView.vue
@@ -0,0 +1,28 @@
+<!-- src\views\FormView.vue -->
+<script setup lang="ts">
+import { ref } from "vue";
+import { JsonForms, type JsonFormsChangeEvent } from "@jsonforms/vue";
+import { vanillaRenderers } from "@jsonforms/vue-vanilla";
+import schema from "./schema.json"
+
+const renderers = Object.freeze([
+  ...vanillaRenderers,
+  // here you can add custom renderers
+]);
+
+const data = ref({});
+
+const onChange = (event: JsonFormsChangeEvent) => {
+  data.value = event.data;
+};
+
+</script>
+
+<template>
+	<json-forms
+		:data="data"
+		:renderers="renderers"
+		:schema="schema"
+		@change="onChange"
+	/>
+</template>
